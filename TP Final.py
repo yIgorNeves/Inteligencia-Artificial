6d51b90611d0dd367e9070bf4ac87cc6afc08231
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2020-10-26 17:18:16 -0300

    IA

---
+++ TP Final.py
@@ -0,0 +1,19 @@
+# Importando Biliotecas Importantes
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importando algumas funções para este código
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from matplotlib.colors import ListedColormap
+from sklearn.metrics import confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read
